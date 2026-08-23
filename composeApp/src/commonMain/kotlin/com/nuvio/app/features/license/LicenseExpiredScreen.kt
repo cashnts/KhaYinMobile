9691c61b55bdcfc2aac7c3fc40c64df2c456ffc8
cashnts <iamaungzayphyo@gmail.com>
2026-08-23 13:59:43 +0630

    fix: imports and coroutines in LicenseExpiredScreen

--- composeApp/src/commonMain/kotlin/com/nuvio/app/features/license/LicenseExpiredScreen.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/features/license/LicenseExpiredScreen.kt
@@ -24,10 +24,16 @@ import androidx.compose.material.icons.rounded.Lock
 import androidx.compose.material.icons.rounded.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -40,6 +46,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nuvio.app.features.settings.AppBrandWordmark
+import kotlinx.coroutines.launch
 
 @Composable
 fun LicenseExpiredScreen(
@@ -212,8 +219,8 @@ fun LicenseExpiredScreen(
                 }
             }
 
-            var isChecking by androidx.compose.runtime.remember { androidx.compose.runtime.mutableStateOf(false) }
-            val coroutineScope = androidx.compose.runtime.rememberCoroutineScope()
+            var isChecking by remember { mutableStateOf(false) }
+            val coroutineScope = rememberCoroutineScope()
 
             Spacer(modifier = Modifier.height(24.dp))
 
@@ -237,7 +244,7 @@ fun LicenseExpiredScreen(
                 ),
             ) {
                 if (isChecking) {
-                    androidx.compose.material3.CircularProgressIndicator(
+                    CircularProgressIndicator(
                         color = Color.Black,
                         modifier = Modifier.size(20.dp),
                         strokeWidth = 2.dp,
